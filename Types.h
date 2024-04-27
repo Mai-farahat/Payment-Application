7ace2b1abc04b73f29a7ee6f004fd5477543e6c2
Mai-farahat <maifarahat00@gmail.com>
2024-04-27 16:28:04 +0300

    first commit

---
+++ Types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_h
+#define TYPES_h
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
+
+typedef signed char uint8_t;
+typedef signed short uint16_t;
+typedef signed long uint32_t;
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "server.h"
+#include "app.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+
+
+
+  appStart();
+
+
+
+     //getCardHolderNameTest();
+     // getCardExpiryDateTest();
+  // getCardPANTest ();
+    // getTransactionDateTest();
+     //isCardExpriedTest();
+    // getTransactionAmountTest();
+   //isBelowMaxAmountTest();
+     //setMaxAmountTest();
+   //  isValidAccountTest();
+   // isBlockedAccountTest();
+    //  isAmountAvailableTest();
+  // saveTransactionTest();
+
+ // recieveTransactionDataTest();
+
+
+    return 0;
+}
